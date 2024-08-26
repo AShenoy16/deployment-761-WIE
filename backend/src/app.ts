45f9081069755f7553a81d7a720a4ec46e3b8802
Aditya Shenoy <97868877+AShenoy16@users.noreply.github.com>
2024-08-27 03:23:42 +1200

    feat: create quiz route and controller
    create initial quiz route and controller

--- backend/src/app.ts
+++ backend/src/app.ts
@@ -3,6 +3,7 @@ import cors from 'cors';
 import dotenv from 'dotenv';
 import connectDB from './config/db';
 import userRoutes from './routes/userRoutes';
+import quizRoutes from './routes/quizRoutes';
 
 dotenv.config();
 
@@ -14,5 +15,6 @@ app.use(cors());
 app.use(express.json());
 
 app.use('/api/users', userRoutes);
+app.use('/api/quiz', quizRoutes);
 
 export default app;
