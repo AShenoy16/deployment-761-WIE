37b7a35e47fc4d00adebde40aa9a6a051f35720c
Aditya Shenoy <97868877+AShenoy16@users.noreply.github.com>
2024-08-27 01:44:07 +1200

    fix: update server connection
    update server connection by establishing db connection

--- backend/src/config/db.ts
+++ backend/src/config/db.ts
@@ -1,12 +1,16 @@
-import mongoose from 'mongoose';
-import dotenv from 'dotenv';
+import mongoose from "mongoose";
+import dotenv from "dotenv";
 
 dotenv.config();
 
 const connectDB = async () => {
   try {
-    await mongoose.connect(process.env.MONGODB_CONNECTION_STRING as string);
-    console.log('MongoDB Connected');
+    // only connect if not alr connected
+    if (mongoose.connection.readyState === 0) {
+      await mongoose.connect(process.env.MONGODB_CONNECTION_STRING as string);
+      console.log("MongoDB Connected");
+      return
+    }
   } catch (error) {
     console.error(`Error: ${error}`);
     process.exit(1);
