8ff9a2cfd34fda4787625997dcc1057b983ff317
Aditya Shenoy <97868877+AShenoy16@users.noreply.github.com>
2024-08-27 01:45:44 +1200

    fix: init server before db connection
    listen to server before init db connection

--- backend/src/server.ts
+++ backend/src/server.ts
@@ -1,17 +1,17 @@
-import app from './app';
-import connectDB from './config/db';
+import app from "./app";
+import connectDB from "./config/db";
 
 const PORT = process.env.PORT || 5000;
 
 const startServer = async () => {
   try {
-    // Establish database connection
-    await connectDB();
-
     // Start the server
     app.listen(PORT, () => {
       console.log(`Server running on port ${PORT}`);
     });
+
+    // Establish database connection
+    await connectDB();
   } catch (error) {
     console.error(`Error: ${error}`);
     process.exit(1);
