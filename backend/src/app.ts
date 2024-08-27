3ada11b09ca4bcf1ef98f148f72a9f730f335511
Aditya Shenoy <97868877+AShenoy16@users.noreply.github.com>
2024-08-27 14:44:12 +1200

    fix: connect to db before running server
    fix order to operations for connecting to db and starting server

--- backend/src/app.ts
+++ backend/src/app.ts
@@ -1,20 +1,17 @@
-import express from 'express';
-import cors from 'cors';
-import dotenv from 'dotenv';
-import connectDB from './config/db';
-import userRoutes from './routes/userRoutes';
-import quizRoutes from './routes/quizRoutes';
+import express from "express";
+import cors from "cors";
+import dotenv from "dotenv";
+import connectDB from "./config/db";
+import userRoutes from "./routes/userRoutes";
+// import quizRouter from './routes/quizRoutes';
 
 dotenv.config();
-
-connectDB();
 
 const app = express();
 
 app.use(cors());
 app.use(express.json());
 
-app.use('/api/users', userRoutes);
-app.use('/api/quiz', quizRoutes);
-
+app.use("/api/users", userRoutes);
+// app.use('/api/quiz', quizRouter);
 export default app;
