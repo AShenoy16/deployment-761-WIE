214e9baacb7e9444d851bee75c525bad318c8326
Mitchell <mitchellwong833@gmail.com>
2024-08-27 17:25:57 +1200

    chore: Install react router dom and for quiz page route

--- frontend/src/App.tsx
+++ frontend/src/App.tsx
@@ -1,31 +1,18 @@
-import axios from "axios";
-import { useQuery } from "@tanstack/react-query";
+import { BrowserRouter, Routes, Route, Navigate } from "react-router-dom";
 import { ThemeProvider } from "@mui/material";
 import { lightTheme } from "./theme";
 import QuizPage from "./pages/QuizPage";
 
 function App() {
-  const fetchPost = async () => {
-    const response = await axios.get(
-      "https://jsonplaceholder.typicode.com/posts/1"
-    );
-    return response.data;
-  };
-
-  const { data, isLoading, error } = useQuery({
-    queryKey: ["post"],
-    queryFn: fetchPost,
-  });
-
-  if (isLoading) return <p>Loading...</p>;
-  if (error) return <p>An error occurred: {error.message}</p>;
-
   return (
     <ThemeProvider theme={lightTheme}>
-      {/* <Box color="#FFF" sx={{ backgroundColor: "primary.main" }}>
-        <p>{data?.title}</p>
-      </Box> */}
-      <QuizPage />
+      <BrowserRouter>
+        <Routes>
+          {/** REMOVE DEFAULT NAVIGATION LATER */}
+          <Route path="/" element={<Navigate to="/quiz" />} />{" "}
+          <Route path="/quiz" element={<QuizPage />} />
+        </Routes>
+      </BrowserRouter>
     </ThemeProvider>
   );
 }
