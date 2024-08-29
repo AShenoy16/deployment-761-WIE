ca5a9cc5ffe6de391923c48b6cf4df2f2d0cf2c3
Mitchell <mitchellwong833@gmail.com>
2024-08-29 16:09:42 +1200

    refactor: Extract header and footer into reusable layout

--- frontend/src/App.tsx
+++ frontend/src/App.tsx
@@ -1,24 +1,21 @@
 import { BrowserRouter, Routes, Route, Navigate } from "react-router-dom";
-import { CssBaseline, ThemeProvider, Stack } from "@mui/material";
+import { CssBaseline, ThemeProvider } from "@mui/material";
 import { lightTheme } from "./theme";
 import QuizPage from "./pages/QuizPage";
-import Navbar from "./components/Navbar";
-import Footer from "./components/Footer";
+import GeneralLayout from "./layouts/GeneralLayout";
 
 function App() {
   return (
     <ThemeProvider theme={lightTheme}>
       <CssBaseline />
-      <Stack minHeight="100vh">
-        <BrowserRouter>
-          <Navbar />
+      <BrowserRouter>
+        <GeneralLayout>
           <Routes>
             <Route path="/" element={<Navigate to="/quiz" />} />
             <Route path="/quiz" element={<QuizPage />} />
           </Routes>
-          <Footer />
-        </BrowserRouter>
-      </Stack>
+        </GeneralLayout>
+      </BrowserRouter>
     </ThemeProvider>
   );
 }
