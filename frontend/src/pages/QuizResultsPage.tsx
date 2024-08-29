71d84de3568035c0405624a609dd2d5f4315ebb9
Jang970 <jang970@aucklanduni.ac.nz>
2024-08-29 23:44:08 +1200

    feat: implemented template for spec cards

--- frontend/src/pages/QuizResultsPage.tsx
+++ frontend/src/pages/QuizResultsPage.tsx
@@ -1,4 +1,16 @@
-import { Stack, Typography } from "@mui/material";
+import {
+  Box,
+  Card,
+  CardContent,
+  Link,
+  List,
+  ListItem,
+  ListItemText,
+  Paper,
+  Stack,
+  Typography,
+} from "@mui/material";
+import { useState } from "react";
 
 const QuizResultsPage = () => {
   return (
@@ -10,11 +22,78 @@ const QuizResultsPage = () => {
         // cards of top 3 recommended specs go here
       }
 
+      <SpecCard />
+
       {
-        // todo: implement download functionality
+        // todo: implement download functionality & error and success alerts
       }
       <Typography>Download Results</Typography>
     </Stack>
+  );
+};
+
+// todo: add props for spec data
+const SpecCard = () => {
+  const [hovered, setHovered] = useState(false);
+
+  const careerPathways = [
+    "Frontend Developer",
+    "Backend Developer",
+    "Full Stack Developer",
+    "DevOps Engineer",
+    "Software Architect",
+  ];
+
+  return (
+    <>
+      <Typography variant="h5">Software Engineering</Typography>
+      <Card
+        onMouseEnter={() => setHovered(true)}
+        onMouseLeave={() => setHovered(false)}
+        sx={{
+          backgroundColor: "#00467F",
+          maxWidth: 250,
+          minHeight: 300,
+          borderRadius: "0.75rem",
+          position: "relative", // Ensure content on top is positioned correctly
+          overflow: "hidden", // Hide any overflow
+          "&:hover": {
+            cursor: "pointer",
+          },
+        }}
+      >
+        <CardContent sx={{ color: "white" }}>
+          <Typography variant="h6">Career Pathways</Typography>
+          <List sx={{ listStyleType: "disc", pl: 1 }}>
+            {careerPathways.map((pathway, index) => (
+              <ListItem key={index} sx={{ display: "list-item", p: 0 }}>
+                <ListItemText primary={pathway} />
+              </ListItem>
+            ))}
+          </List>
+        </CardContent>
+        {hovered && (
+          <Box
+            sx={{
+              position: "absolute",
+              top: 0,
+              left: 0,
+              width: "100%",
+              height: "100%",
+              backgroundColor: "rgba(0, 0, 0, 0.7)", // Semi-transparent overlay
+              color: "white",
+              display: "flex",
+              flexDirection: "column",
+              justifyContent: "center",
+              alignItems: "center",
+            }}
+          >
+            <Typography>Software Engineers are problem solvers</Typography>
+            <Link href="/">Click to find out more</Link>
+          </Box>
+        )}
+      </Card>
+    </>
   );
 };
 
