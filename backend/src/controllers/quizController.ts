22ec8c65de955e7d5c8a77b1359f68c67ee088d2
Aditya Shenoy <97868877+AShenoy16@users.noreply.github.com>
2024-08-30 19:21:30 +1200

    feat: validate quiz submission
    ensure quiz submission is of the correct type

--- backend/src/controllers/quizController.ts
+++ backend/src/controllers/quizController.ts
@@ -1,6 +1,7 @@
 import { Request, Response } from "express";
 import { QuizSubmissionRequest } from "../types/quizTypes";
 import { processQuizSubmission } from "../services/quizService";
+import { isQuizSubmissionRequest } from "../validation/quizValidation";
 
 export const getQuiz = async (req: Request, res: Response) => {
   return res.status(200).json({ message: "TODO implement quiz endpoints" });
@@ -8,12 +9,19 @@ export const getQuiz = async (req: Request, res: Response) => {
 
 /**
  * Controller that will process quiz submission and return top three results
+ * assuming validation has been done on the frontend and the types are correct
  * @param req 
  * @param res 
  */
 export const submitQuiz = async (req: Request, res: Response) => {
   try {
-    const quizSubmission: QuizSubmissionRequest = req.body.quizSubmission;
+    const quizSubmission: QuizSubmissionRequest = req.body;
+
+    // missing info/incorrect info
+    if (!isQuizSubmissionRequest(quizSubmission)) {
+      return res.status(422).json({ message: "Incorrect Information" });
+    }
+    
     const result = await processQuizSubmission(quizSubmission);
     res.status(200).json(result);
   } catch (error) {
