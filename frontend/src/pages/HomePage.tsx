624ecd5963485033806c3f3f6414cdc1f0bb18ee
Kai Hirafune <khir829@aucklanduni.ac.nz>
2024-08-30 14:39:39 +1200

    feat: add temporary quiz button on home page

--- frontend/src/pages/HomePage.tsx
+++ frontend/src/pages/HomePage.tsx
@@ -1,7 +1,19 @@
 import React from "react";
+import { useNavigate } from "react-router-dom";
 
-const HomePage = () => {
-  return <div>HomePage</div>;
+const HomePage: React.FC = () => {
+  const navigate = useNavigate();
+
+  const handleQuizButtonClick = () => {
+    navigate("/quiz");
+  };
+
+  return (
+    <div>
+      <p>HomePage</p>
+      <button onClick={handleQuizButtonClick}>Temporary quiz button</button>
+    </div>
+  );
 };
 
 export default HomePage;
