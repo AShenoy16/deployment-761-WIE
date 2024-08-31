13eb66c7791d055ffd24038a296dd4abacba11e3
Aditya Shenoy <97868877+AShenoy16@users.noreply.github.com>
2024-08-31 19:06:07 +1200

    fix: validation errors
    fixed error where would over-validate submission by checking if mcq, slider, ranking all present

--- backend/src/validation/quizValidation.ts
+++ backend/src/validation/quizValidation.ts
@@ -9,9 +9,6 @@ import { QuizSubmissionRequest } from '../types/quizTypes';
 export function isQuizSubmissionRequest(obj: any): obj is QuizSubmissionRequest {
   return (
     typeof obj === 'object' &&  // Check if obj is an object or null
-    Object.keys(obj).length !== 0 && // check len not equal to zero
-    typeof obj.mcqAnswers === 'object' && Object.keys(obj.mcqAnswers).length !== 0 &&
-    typeof obj.sliderAnswers === 'object' &&  Object.keys(obj.sliderAnswers).length !== 0 &&
-    typeof obj.rankingAnswers === 'object' &&  Object.keys(obj.rankingAnswers).length !== 0
+    Object.keys(obj).length !== 0 // check len not equal to zero
   );
 }
