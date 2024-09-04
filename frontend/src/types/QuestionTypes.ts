8d022b414a2a23a4a77cc405b262895d80dfceba
Mitchell <mitchellwong833@gmail.com>
2024-09-04 23:41:30 +1200

    refactor: Changed question types

--- frontend/src/types/QuestionTypes.ts
+++ frontend/src/types/QuestionTypes.ts
@@ -1,58 +1,63 @@
-// MCQ types
-export type MCQAnswerOption = {
-  optionId: string;
+// MCQ interfaces
+export interface IMCQAnswerOption {
+  _id: string;
   text: string;
   weightings: { [specializationName: string]: number };
-};
+}
 
-export type MCQQuestion = {
-  type: "mcq";
+export interface IMCQQuestion {
+  _id: string;
+  questionType: "MCQ";
   questionText: string;
-  questionNumber: number;
-  answerOptions: MCQAnswerOption[];
+  answerOptions: IMCQAnswerOption[];
   createdAt: Date;
   updatedAt: Date;
-};
+}
 
-// Ranking types
-export type RankingAnswerOption = {
-  optionId: string;
+// Ranking interfaces
+export interface IRankingWeights {
+  _id: string;
+  specializationName: string;
+  weights: { [rank: string]: number };
+}
+
+export interface IRankingAnswerOption {
+  _id: string;
   text: string;
-  weightings: { [specializationName: string]: { [rank: number]: number } };
-};
+  weightings: IRankingWeights[];
+}
 
-export type RankingQuestion = {
-  type: "ranking";
+export interface IRankingQuestion {
+  _id: string;
+  questionType: "Ranking";
   questionText: string;
-  questionNumber: number;
-  answerOptions: RankingAnswerOption[];
+  answerOptions: IRankingAnswerOption[];
   createdAt: Date;
   updatedAt: Date;
-};
+}
 
-// Slider types
-export type SliderRange = {
-  sliderId: string;
+// Slider interfaces
+export interface ISliderRange {
   min: number;
   max: number;
   weightings: { [specializationName: string]: number[] };
-};
+}
 
-export type SliderQuestion = {
-  type: "slider";
+export interface ISliderQuestion {
+  _id: string;
+  questionType: "Slider";
   questionText: string;
-  questionNumber: number;
-  sliderRange: SliderRange;
+  sliderRange: ISliderRange;
   createdAt: Date;
   updatedAt: Date;
-};
+}
 
 // General quiz types
-export type Question = MCQQuestion | RankingQuestion | SliderQuestion;
+export type IQuestion = IMCQQuestion | IRankingQuestion | ISliderQuestion;
 
 export type Quiz = {
   quizName: string;
-  quizQuestions: Question[];
+  quizQuestions: IQuestion[];
   createdAt: Date;
   updatedAt: Date;
 };
