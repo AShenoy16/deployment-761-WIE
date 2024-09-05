9e15bc4a9254f31eca479660b673067ed3e62798
Aditya Shenoy <97868877+AShenoy16@users.noreply.github.com>
2024-09-06 00:40:03 +1200

    feat: created initial login component

--- frontend/src/pages/LoginPage.tsx
+++ frontend/src/pages/LoginPage.tsx
@@ -1,18 +1,50 @@
 import React from "react";
 import { useNavigate } from "react-router-dom";
+import { useTheme } from '@mui/material/styles';
+import {AuthProvider, AppProvider, SignInPage, AuthResponse } from '@toolpad/core';
 
 const LoginPage: React.FC = () => {
   const navigate = useNavigate();
 
-  const handleQuizButtonClick = () => {
-    navigate("/quiz");
+  const handleSuccessfulLogin = () => {
+    navigate("/");
   };
 
+  const providers = [{ id: 'credentials', name: 'Email and Password' }];
+
+  const signIn: (
+    provider: AuthProvider,
+    formData?: FormData,
+    callbackUrl?: string
+  ) => Promise<AuthResponse> | void = async (provider, formData, callbackUrl) => {
+    const promise = new Promise<AuthResponse>((resolve) => {
+      setTimeout(() => {
+        const email = formData?.get('email');
+        const password = formData?.get('password');
+        alert(
+          `Signing in with "${provider.name}" and credentials: ${email}, ${password}`,
+        );
+        redirectTo: callbackUrl
+        // preview-start
+        resolve({
+          type: 'CredentialsSignin',
+          error: 'Invalid Credentials'
+        });
+
+        handleSuccessfulLogin()
+        // preview-end
+      }, 300);
+    });
+    return promise;
+  };
+
+
+  const theme = useTheme();
+
   return (
-    <div>
-      <p>Login</p>
-      <button onClick={handleQuizButtonClick}>Login</button>
-    </div>
+    <AppProvider theme={theme}>
+      <SignInPage signIn={signIn} providers={providers}/>
+    </AppProvider>
   );
 };
 
