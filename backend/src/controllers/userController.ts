9033159cd419dc9dfa49754317d3df2a71cfd0a4
Aditya Shenoy <97868877+AShenoy16@users.noreply.github.com>
2024-09-07 00:32:56 +1200

    feat: created route and endpoint to login user

--- backend/src/controllers/userController.ts
+++ backend/src/controllers/userController.ts
@@ -1,5 +1,27 @@
 import { Request, Response } from "express";
+import { loginAdmin } from "../services/authentication";
 
 export const getUsers = async (req: Request, res: Response) => {
   return res.status(200).json({ message: "create user endpoints" });
+};
+
+/**
+ * Endpoint to login user
+ * @param req 
+ * @param res 
+ * @returns 
+ */
+export const loginUsers = async (req: Request, res: Response) => {
+  try {
+    const { email, password } = req.body;
+    const admin = await loginAdmin(email, password);
+
+    if (!admin) {
+      return res.status(401).json({ message: "Unauthorized access" });
+    }
+
+    return res.status(200).json();
+  } catch (error) {
+    return res.status(500);
+  }
 };
