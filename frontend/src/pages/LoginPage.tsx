50027c6bf94310ef431fe23d0cda8975bb0c0270
Aditya Shenoy <97868877+AShenoy16@users.noreply.github.com>
2024-09-06 23:56:49 +1200

    feat: frontend login functionality
    created initial login functionality for the frontend that calls backend and shows errors

--- frontend/src/pages/LoginPage.tsx
+++ frontend/src/pages/LoginPage.tsx
@@ -1,49 +1,68 @@
 import React from "react";
 import { useNavigate } from "react-router-dom";
-import { useTheme } from '@mui/material/styles';
-import {AuthProvider, AppProvider, SignInPage, AuthResponse } from '@toolpad/core';
+import { useTheme } from "@mui/material/styles";
+import {
+  AuthProvider,
+  AppProvider,
+  SignInPage,
+  AuthResponse,
+} from "@toolpad/core";
 
 const LoginPage: React.FC = () => {
   const navigate = useNavigate();
 
+  // navigate to home page
   const handleSuccessfulLogin = () => {
+    // set state to true
     navigate("/");
   };
 
-  const providers = [{ id: 'credentials', name: 'Email and Password' }];
+  const providers = [{ id: "credentials", name: "Email and Password" }];
 
   const signIn: (
     provider: AuthProvider,
-    formData?: FormData,
-    callbackUrl?: string
-  ) => Promise<AuthResponse> | void = async (provider, formData, callbackUrl) => {
+    formData?: FormData
+  ) => Promise<AuthResponse> = async (_provider, formData) => {
     const promise = new Promise<AuthResponse>((resolve) => {
-      setTimeout(() => {
-        const email = formData?.get('email');
-        const password = formData?.get('password');
-        alert(
-          `Signing in with "${provider.name}" and credentials: ${email}, ${password}`,
-        );
-        redirectTo: callbackUrl
-        // preview-start
-        resolve({
-          type: 'CredentialsSignin',
-          error: 'Invalid Credentials'
-        });
+      const email = formData?.get("email");
+      const password = formData?.get("password");
 
-        handleSuccessfulLogin()
-        // preview-end
-      }, 300);
+      fetch("http://localhost:5000/api/users", {
+        method: "POST",
+        headers: {
+          "Content-Type": "application/json",
+        },
+        body: JSON.stringify({ email, password }),
+      })
+        .then((response) => {
+          // successful login
+          if (response.ok) {
+            handleSuccessfulLogin();
+            resolve({});
+          } else {
+            // error during login
+            resolve({
+              type: "CredentialsSignin",
+              error: "Invalid Credentials",
+            });
+          }
+        })
+        // network errors
+        .catch(() => {
+          resolve({
+            type: "CredentialsSignin",
+            error: "Network Error",
+          });
+        });
     });
     return promise;
   };
-
 
   const theme = useTheme();
 
   return (
     <AppProvider theme={theme}>
-      <SignInPage signIn={signIn} providers={providers}/>
+      <SignInPage signIn={signIn} providers={providers} />
     </AppProvider>
   );
 };
