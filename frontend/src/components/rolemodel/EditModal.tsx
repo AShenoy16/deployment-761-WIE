715c7b03be3615c2d184a1fa5cf46768a17b0c1a
sli521 <sli521@aucklanduni.ac.nz>
2024-09-06 14:53:42 +1200

    refactor: role model cards in edit modal refactored into a component

---
+++ frontend/src/components/rolemodel/EditModal.tsx
@@ -0,0 +1,98 @@
+import { Box, Typography, IconButton, Avatar } from "@mui/material";
+import { RoleModel } from "../../types/RoleModel";
+import CloseIcon from "@mui/icons-material/Close";
+import EditNoteIcon from "@mui/icons-material/EditNote";
+import LinkedInIcon from "@mui/icons-material/LinkedIn";
+import InstagramIcon from "@mui/icons-material/Instagram";
+
+const EditModal: React.FC<RoleModel> = (roleModel: RoleModel) => {
+  return (
+    <Box
+      sx={{
+        display: "flex",
+        flexDirection: "column",
+        marginBottom: 5,
+        bgcolor: "secondary.main",
+        padding: 1,
+        borderRadius: 5,
+      }}
+    >
+      {/* First Row Layer */}
+      <Box
+        sx={{
+          display: "flex",
+          justifyContent: "space-between",
+          alignItems: "center",
+        }}
+      >
+        <IconButton>
+          <CloseIcon />
+        </IconButton>
+        <IconButton>
+          <EditNoteIcon />
+        </IconButton>
+      </Box>
+
+      {/* Second Row Layer */}
+      <Box
+        sx={{
+          display: "flex",
+          justifyContent: "space-between",
+          padding: 3,
+          paddingTop: 0,
+        }}
+      >
+        {/* Left Column */}
+        <Box
+          sx={{
+            display: "flex",
+            flexDirection: "column",
+            alignItems: "center",
+            justifyContent: "center",
+          }}
+        >
+          <Avatar
+            alt={roleModel.name}
+            src={roleModel.photoUrl}
+            sx={{ width: 200, height: 200 }}
+          />
+        </Box>
+
+        {/* Right Column */}
+        <Box
+          sx={{
+            display: "flex",
+            flexDirection: "column",
+            justifyContent: "start",
+            flex: 1,
+            ml: 7,
+            color: "white",
+          }}
+        >
+          <Typography variant="h6">{roleModel.name}</Typography>
+          <Typography variant="body1">{roleModel.description}</Typography>
+
+          {/* Social Media Links */}
+          <Box display="flex" alignItems="center" marginLeft={-1}>
+            <IconButton
+              component="a"
+              href={roleModel.socialMediaLinks.linkedin}
+              target="_blank"
+            >
+              <LinkedInIcon />
+            </IconButton>
+            <IconButton
+              component="a"
+              href={roleModel.socialMediaLinks.instagram}
+              target="_blank"
+            >
+              <InstagramIcon />
+            </IconButton>
+          </Box>
+        </Box>
+      </Box>
+    </Box>
+  );
+};
+
+export default EditModal;
--- frontend/src/pages/RoleModelsPage.tsx
+++ frontend/src/pages/RoleModelsPage.tsx
@@ -1,19 +1,9 @@
 import React, { useState } from "react";
-import {
-  Button,
-  Modal,
-  Box,
-  Typography,
-  IconButton,
-  Avatar,
-} from "@mui/material";
+import { Button, Modal, Box, Typography, IconButton } from "@mui/material";
 import ArrowBackIcon from "@mui/icons-material/ArrowBack";
 import AddIcon from "@mui/icons-material/Add";
-import CloseIcon from "@mui/icons-material/Close";
-import EditNoteIcon from "@mui/icons-material/EditNote";
-import LinkedInIcon from "@mui/icons-material/LinkedIn";
-import InstagramIcon from "@mui/icons-material/Instagram";
 import { RoleModel } from "../types/RoleModel";
+import EditModal from "../components/rolemodel/EditModal";
 
 // Define modal styles
 const modalStyle = {
@@ -143,93 +133,7 @@ const RoleModelsPage: React.FC = () => {
             }}
           >
             {mockRoleModelData.map((roleModel, index) => (
-              <Box
-                key={index}
-                sx={{
-                  display: "flex",
-                  flexDirection: "column",
-                  marginBottom: 5,
-                  bgcolor: "secondary.main",
-                  padding: 1,
-                  borderRadius: 5,
-                }}
-              >
-                {/* First Row Layer */}
-                <Box
-                  sx={{
-                    display: "flex",
-                    justifyContent: "space-between",
-                    alignItems: "center",
-                  }}
-                >
-                  <IconButton>
-                    <CloseIcon />
-                  </IconButton>
-                  <IconButton>
-                    <EditNoteIcon />
-                  </IconButton>
-                </Box>
-
-                {/* Second Row Layer */}
-                <Box
-                  sx={{
-                    display: "flex",
-                    justifyContent: "space-between",
-                    padding: 3,
-                    paddingTop: 0,
-                  }}
-                >
-                  {/* Left Column */}
-                  <Box
-                    sx={{
-                      display: "flex",
-                      flexDirection: "column",
-                      alignItems: "center",
-                      justifyContent: "center",
-                    }}
-                  >
-                    <Avatar
-                      alt={roleModel.name}
-                      src={roleModel.photoUrl}
-                      sx={{ width: 200, height: 200 }}
-                    />
-                  </Box>
-
-                  {/* Right Column */}
-                  <Box
-                    sx={{
-                      display: "flex",
-                      flexDirection: "column",
-                      justifyContent: "start",
-                      flex: 1,
-                      ml: 7,
-                    }}
-                  >
-                    <Typography variant="h6">{roleModel.name}</Typography>
-                    <Typography variant="body1">
-                      {roleModel.description}
-                    </Typography>
-
-                    {/* Social Media Links */}
-                    <Box display="flex" alignItems="center" marginLeft={-1}>
-                      <IconButton
-                        component="a"
-                        href={roleModel.socialMediaLinks.linkedin}
-                        target="_blank"
-                      >
-                        <LinkedInIcon />
-                      </IconButton>
-                      <IconButton
-                        component="a"
-                        href={roleModel.socialMediaLinks.instagram}
-                        target="_blank"
-                      >
-                        <InstagramIcon />
-                      </IconButton>
-                    </Box>
-                  </Box>
-                </Box>
-              </Box>
+              <EditModal key={index} {...roleModel} />
             ))}
           </Box>
           <Box sx={{ display: "flex", justifyContent: "center" }}>
