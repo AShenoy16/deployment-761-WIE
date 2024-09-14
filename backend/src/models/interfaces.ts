3fa7916db98f07b2a53a08167d2ce20f91c97ff0
Aditya Shenoy <97868877+AShenoy16@users.noreply.github.com>
2024-09-14 17:39:41 +1200

    fix: fixed backend interfaces to match new models

--- backend/src/models/interfaces.ts
+++ backend/src/models/interfaces.ts
@@ -7,7 +7,7 @@ export interface IMCQAnswerOption {
 }
 
 export interface IMCQQuestion extends Document {
-  questionType: "MCQ";
+  questionType: "mcq";
   questionText: string;
   _id: string;
   answerOptions: IMCQAnswerOption[];
@@ -16,22 +16,15 @@ export interface IMCQQuestion extends Document {
 }
 
 // Ranking interfaces
-
-export interface IRankingWeights {
-  _id: string; // ObjectId in string format
-  specializationName: string;
-  weights: { [rank: string]: number }; // Map of rank (string) to weight (number)
-}
-
 export interface IRankingAnswerOption {
   text: string;
   _id: string;
-  // map of rank to rank to weight
-  weightings: IRankingWeights[]; // Array of weightings
+  // map of specName to currentWeight
+  weightings: { [specializationName: string]: number };
 }
 
 export interface IRankingQuestion {
-  questionType: "Ranking";
+  questionType: "ranking";
   questionText: string;
   answerOptions: IRankingAnswerOption[];
   createdAt: Date;
@@ -40,17 +33,16 @@ export interface IRankingQuestion {
 
 // Slider interfaces
 
-export interface ISliderRange {
-  min: number;
-  max: number;
-  weightings: { [specializationName: string]: number[] };
+export interface ISliderWeights {
+  _id: string;
+  weightings: { [specializationName: string]: number };
 }
 
 export interface ISliderQuestion extends Document {
-  questionType: "Slider";
+  questionType: "slider";
   questionText: string;
   _id: string;
-  sliderRange: ISliderRange;
+  sliderRange: ISliderWeights;
   createdAt: Date;
   updatedAt: Date;
 }
