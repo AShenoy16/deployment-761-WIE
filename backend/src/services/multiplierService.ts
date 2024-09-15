6f64fc67b49ad4ce3c3e3e7499106f7b2894fcc3
Aditya Shenoy <97868877+AShenoy16@users.noreply.github.com>
2024-09-15 19:12:40 +1200

    feat: create method to validate multiplier type

--- backend/src/services/multiplierService.ts
+++ backend/src/services/multiplierService.ts
@@ -1,13 +1,24 @@
+import { IMultiplierData } from "../models/interfaces";
 import MultiplierData from "../models/multiplerModel";
 
 // get the multiplier information
 export const getAllMultipliers = async () => {
-    try {
-      const multipliers = await MultiplierData.find().select('_id rank3Multiplier rank2Multiplier sliderFactor');
-      return multipliers;
-    } catch (error) {
-      console.error('Error fetching multipliers:', error);
-      throw error;
-    }
-  };
-  
+  try {
+    const multipliers = await MultiplierData.find().select(
+      "_id rank3Multiplier rank2Multiplier sliderFactor"
+    );
+    return multipliers;
+  } catch (error) {
+    console.error("Error fetching multipliers:", error);
+    throw error;
+  }
+};
+
+export const isValidMultiplier = (multiplier: IMultiplierData): boolean => {
+  return (
+    typeof multiplier._id === "string" &&
+    typeof multiplier.rank2Multiplier === "number" &&
+    typeof multiplier.rank3Multiplier === "number" &&
+    typeof multiplier.sliderFactor === "number"
+  );
+};
