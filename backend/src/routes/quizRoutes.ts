a071337bf494715b550a4b3fb7aed9d11796fa7b
sli521 <sli521@aucklanduni.ac.nz>
2024-09-15 20:20:15 +1200

    feat: quiz route for adding new quiz content

--- backend/src/routes/quizRoutes.ts
+++ backend/src/routes/quizRoutes.ts
@@ -1,9 +1,14 @@
-import express from 'express';
-import { submitQuiz, getQuiz } from '../controllers/quizController';
+import express from "express";
+import {
+  submitQuiz,
+  getQuiz,
+  postQuizContent,
+} from "../controllers/quizController";
 
 const router = express.Router();
 
-router.get('/', getQuiz);
-router.post('/', submitQuiz)
+router.get("/", getQuiz);
+router.post("/submit", submitQuiz);
+router.post("/", postQuizContent);
 
 export default router;
