72f392a3e8c140be008ce6147e0e61c2a419a50a
Jang970 <jang970@aucklanduni.ac.nz>
2024-09-17 19:48:03 +1200

    feat: implemented initial UI layout and conditional edit button

--- frontend/src/pages/HighschoolRequirementsPage.tsx
+++ frontend/src/pages/HighschoolRequirementsPage.tsx
@@ -1,10 +1,50 @@
-import { Box, Typography } from "@mui/material";
+import { Box, Button, Container, Typography } from "@mui/material";
+import { useAuthStore } from "../stores/AuthenticationStore";
+
+const buttonStyle = {
+  textTransform: "none",
+  textDecorationLine: "underline",
+  borderRadius: "12px",
+};
 
 const HighschoolRequirementsPage = () => {
+  const isAdminLoggedIn = useAuthStore((state) => state.isLoggedIn);
+
   return (
-    <Box>
-      <Typography>Highschool Requirements</Typography>
-    </Box>
+    <Container>
+      <Typography
+        variant="h4"
+        component="h1"
+        gutterBottom
+        marginY={4}
+        fontWeight="bold"
+        sx={{ color: "#00467F" }}
+      >
+        Highschool Entry Requirements
+      </Typography>
+      <Typography
+        variant="h5"
+        component="h1"
+        mb={4}
+        gutterBottom
+        fontWeight="bold"
+        sx={{ color: "#00467F" }}
+      >
+        You are guaranteed entry to the Engineering programme if you meet these
+        requirements:
+      </Typography>
+
+      {/* Edit Button */}
+      {isAdminLoggedIn && (
+        <Box
+          sx={{ display: "flex", justifyContent: "center", marginBottom: 3 }}
+        >
+          <Button variant="contained" color="secondary" sx={buttonStyle}>
+            Edit
+          </Button>
+        </Box>
+      )}
+    </Container>
   );
 };
 
