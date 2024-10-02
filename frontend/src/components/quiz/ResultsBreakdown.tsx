8aa06e86aaa1d3ba2f2a5f67fc589b955ada1280
sli521 <sli521@aucklanduni.ac.nz>
2024-10-03 10:25:34 +1300

    feat: snackbar added for saving results

--- frontend/src/components/quiz/ResultsBreakdown.tsx
+++ frontend/src/components/quiz/ResultsBreakdown.tsx
@@ -4,10 +4,14 @@ import {
   useTheme,
   useMediaQuery,
   Button,
+  Snackbar,
+  Alert,
 } from "@mui/material";
 import PieChart from "./PieChart";
 import React from "react";
 import { usePDF } from "react-to-pdf";
+import { useSnackbarStore } from "../../stores/SnackBarStore";
+import useSnackBar from "../../hooks/useSnackBar";
 
 type ResultsBreakdownProps = {
   pieChartData: { name: string; score: number }[];
@@ -19,7 +23,23 @@ const ResultsBreakdown: React.FC<ResultsBreakdownProps> = ({
   const theme = useTheme();
   const isSmallScreen = useMediaQuery(theme.breakpoints.down("sm")); // Detect small screens
 
-  const { toPDF, targetRef } = usePDF({ filename: "page.pdf" });
+  const message = useSnackbarStore((state) => state.message);
+  const isOpen = useSnackbarStore((state) => state.isOpen);
+  const severity = useSnackbarStore((state) => state.severity);
+  const setIsOpen = useSnackbarStore((state) => state.setIsOpen);
+  const handleSnackBarClose = (): void => setIsOpen(false);
+  const showSnackbar = useSnackBar();
+
+  const { toPDF, targetRef } = usePDF({ filename: "quiz_results.pdf" });
+
+  const saveResultsAsPDF = () => {
+    try {
+      toPDF();
+      showSnackbar("Successfully downloaded results as PDF", true);
+    } catch {
+      showSnackbar("Error saving results as PDF", false);
+    }
+  };
 
   return (
     <Box textAlign="center">
@@ -30,8 +50,17 @@ const ResultsBreakdown: React.FC<ResultsBreakdownProps> = ({
         <PieChart data={pieChartData} />
       </Box>
       <Box>
-        <Button onClick={() => toPDF()}>Download Results</Button>
+        <Button onClick={() => saveResultsAsPDF()}>Download Results</Button>
       </Box>
+      <Snackbar
+        open={isOpen}
+        autoHideDuration={5000}
+        onClose={handleSnackBarClose}
+      >
+        <Alert onClose={() => setIsOpen(false)} severity={severity}>
+          {message}
+        </Alert>
+      </Snackbar>
     </Box>
   );
 };
