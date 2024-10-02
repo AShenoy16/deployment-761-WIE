1e8cef2fa4fd89a9fadeb1ed271fbcc90813d210
sli521 <sli521@aucklanduni.ac.nz>
2024-10-03 10:13:54 +1300

    feat: added save results as pdf functionality for results breakdown

--- frontend/src/components/quiz/ResultsBreakdown.tsx
+++ frontend/src/components/quiz/ResultsBreakdown.tsx
@@ -1,5 +1,13 @@
-import { Box, Typography, useTheme, useMediaQuery } from "@mui/material";
+import {
+  Box,
+  Typography,
+  useTheme,
+  useMediaQuery,
+  Button,
+} from "@mui/material";
 import PieChart from "./PieChart";
+import React from "react";
+import { usePDF } from "react-to-pdf";
 
 type ResultsBreakdownProps = {
   pieChartData: { name: string; score: number }[];
@@ -11,15 +19,19 @@ const ResultsBreakdown: React.FC<ResultsBreakdownProps> = ({
   const theme = useTheme();
   const isSmallScreen = useMediaQuery(theme.breakpoints.down("sm")); // Detect small screens
 
+  const { toPDF, targetRef } = usePDF({ filename: "page.pdf" });
+
   return (
     <Box textAlign="center">
       <Typography variant={isSmallScreen ? "h4" : "h2"} mb={2}>
         Results Breakdown
       </Typography>
-      <PieChart data={pieChartData} />
-      <Typography variant={isSmallScreen ? "body1" : "h6"} mt={2}>
-        Download Results
-      </Typography>
+      <Box ref={targetRef}>
+        <PieChart data={pieChartData} />
+      </Box>
+      <Box>
+        <Button onClick={() => toPDF()}>Download Results</Button>
+      </Box>
     </Box>
   );
 };
