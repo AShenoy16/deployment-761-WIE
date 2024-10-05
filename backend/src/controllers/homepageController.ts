4c44a2a79439c61edef1fce4b933339dc0f1b2e0
Kai Hirafune <khir829@aucklanduni.ac.nz>
2024-10-05 17:31:11 +1300

    refactor: remove unnecessary comment

--- backend/src/controllers/homepageController.ts
+++ backend/src/controllers/homepageController.ts
@@ -50,39 +50,3 @@ export const updateHomePage = async (req: Request, res: Response) => {
 		return res.status(500).json({ message: "Server error" });
 	}
 };
-
-// // Update the home page data
-// export const updateHomePage = async (req: Request, res: Response) => {
-// 	try {
-// 		const homePage = await HomePage.findOne();
-// 		if (!homePage) {
-// 			return res.status(404).json({ message: "Home page content not found" });
-// 		}
-
-// 		const { heroTitle, heroSubtitle, heroImage, section1Header, section1Text, section2Header, section2Text, additionalResources } = req.body;
-
-// 		homePage.heroTitle = heroTitle || homePage.heroTitle;
-// 		homePage.heroSubtitle = heroSubtitle || homePage.heroSubtitle;
-// 		// homePage.heroImage = heroImage || homePage.heroImage;
-// 		homePage.section1Header = section1Header || homePage.section1Header;
-// 		homePage.section1Text = section1Text || homePage.section1Text;
-// 		homePage.section2Header = section2Header || homePage.section2Header;
-// 		homePage.section2Text = section2Text || homePage.section2Text;
-// 		homePage.additionalResources = additionalResources || homePage.additionalResources;
-
-// 		console.log(req.files);
-// 		// Handle image uploads
-// 		const files = req.files as { [fieldname: string]: MulterFile[] };
-
-// 		if (files?.heroImage) {
-// 			const heroImage = files["heroImage"][0];
-// 			homePage.heroImage = `/uploads/${heroImage.filename}`;
-// 		}
-
-// 		await homePage.save();
-// 		return res.status(200).json(homePage);
-// 	} catch (error) {
-// 		console.error("Error updating home page:", error);
-// 		return res.status(500).json({ message: "Server error" });
-// 	}
-// };
